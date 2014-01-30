aabb29a7d23706e2db0582ae7da418d4787be75f
Alex <alex@gmail.com>
2014-01-30 19:49:31 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html class="no-js">
+<head>
+	<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;" />
+	<meta charset="utf-8" />
+	<title>Git Test</title>
+</head>
+<body>
+	<h1>Git Test</h1>
+</body>
+</html>
