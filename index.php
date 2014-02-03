c5dec7cff9388212f73dace17c4b4309416214a1
Alex <alex@gmail.com>
2014-02-03 18:05:37 +0000

    added links to the stylesheets

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
 	<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;" />
 	<meta charset="utf-8" />
 	<title>Git Test</title>
+	<link rel="stylesheet" href="css/normalize.css">
+	<link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
 	<h1>Git Test</h1>
