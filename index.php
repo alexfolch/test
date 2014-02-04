cd95f829f48f3797627a79c1dfc3c4e9dc30e70c
Alex <alex@gmail.com>
2014-02-04 16:12:59 +0000

    Created navigation

--- index.php
+++ index.php
@@ -9,6 +9,14 @@
 </head>
 <body>
 	<h1>Git Test</h1>
+	<nav>
+		<ul>
+			<li><a href="index.php">Home</a></li>
+			<li><a href="portfolio.php">Portfolio</a></li>
+			<li><a href="about.php">About</a></li>
+			<li><a href="contact.php">Contact</a></li>
+		</ul>
+	</nav>
 	<span>Here's a list</span>
 	<ol>
 		<li>One thing</li>
